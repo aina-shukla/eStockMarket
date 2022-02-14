58fd56e684aa02217477096344b8838f980551a0
aina-shukla <shuklaaina@gmail.com>
2022-02-14 19:50:55 +0530

    Adding cross origin

--- company-service/src/main/java/com/demo/controllers/CompanyController.java
+++ company-service/src/main/java/com/demo/controllers/CompanyController.java
@@ -3,6 +3,7 @@ package com.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import com.demo.entities.CompanyEntity;
 import com.demo.services.CompanyService;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class CompanyController {
 
 	@Autowired
--- stock-service/src/main/java/com/stockService/com/stockService/controllers/StockController.java
+++ stock-service/src/main/java/com/stockService/com/stockService/controllers/StockController.java
@@ -2,6 +2,7 @@ package com.stockService.com.stockService.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import com.stockService.com.stockService.entity.StockEntity;
 import com.stockService.com.stockService.services.StockService;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class StockController {
 
 	@Autowired
